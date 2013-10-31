d5d8c608c60c352edfb88aeec682f9ae0e6362d2
Thúlio Costa <thulio@thuliocosta.com>
2013-10-31 14:49:59 -0200

    Adding package

---
+++ notify_plugin/notify_plugin.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+"""
+Notify any PROCESS_STATE changes in realtime
+to the dashboard server
+"""
+import os
+import sys
+
+import requests
+
+from supervisor import childutils
+
+
+def main():
+
+    if not "SUPERVISOR_SERVER_URL" in os.environ:
+        sys.stderr.write("notify_plugin must be run as a supervisor event listener\n")
+        sys.stderr.flush()
+        return
+
+    while 1:
+        headers, payload = childutils.listener.wait(sys.stdin, sys.stdout)
+        sys.stderr.write("{}\n\n".format(headers))
+        sys.stderr.write("{}\n\n".format(payload))
+        #TODO: Send a POST to the Dashboard URL
+        # requests.post(DASHBOARD_URL, data={"key": "value"}
+        childutils.listener.ok(sys.stdout)
+
+
+if __name__ == "__main__":
+    main()
