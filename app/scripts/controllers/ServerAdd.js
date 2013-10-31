240687c188e49daa8f88c726e5923c2d466fdc56
Dalton Barreto <daltonmatos@gmail.com>
2013-10-31 18:53:59 -0200

    Fix na msg de feedback quando salvamos um novo server

--- app/scripts/controllers/ServerAdd.js
+++ app/scripts/controllers/ServerAdd.js
@@ -4,17 +4,18 @@ angular.module('dashSupervisorFrontApp')
   .controller('ServeraddCtrl', function ($scope, $rootScope, $http) {
       $rootScope.activate("Servidores");
       $scope.title = "Addind a new Server";
-      $scope.server_added = {};
 
       $scope.reset = function (){
           $scope.server = {};
       };
 
       $scope.save = function(server){
-          console.log(server);
-          $scope.server_added = $http({method: "POST",
-                                            url:"http://localhost:3000/server",
-                                            data: angular.toJson(server)}
-          );
+          $http({method: "POST",url:"http://localhost:3000/server", data: angular.toJson(server)})
+              .success(function (data, status){
+                    $scope.add_status = status;
+              })
+              .error(function (data, status){
+                    $scope.add_status = status;
+              });
       }
   });
