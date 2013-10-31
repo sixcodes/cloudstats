d25c083663d023875473595e8bdb831c417caa1d
Dalton Barreto <daltonmatos@gmail.com>
2013-10-31 15:54:18 -0200

    Basic form validation and ng-model

--- app/scripts/controllers/ServerAdd.js
+++ app/scripts/controllers/ServerAdd.js
@@ -4,4 +4,13 @@ angular.module('dashSupervisorFrontApp')
   .controller('ServeraddCtrl', function ($scope, $rootScope) {
   $rootScope.activate("Servidores");
   $scope.title = "Addind a new Server";
+
+
+  $scope.reset = function (){
+
+  }
+
+  $scope.save = function(server){
+      console.log(server);
+  }
   });
