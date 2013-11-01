4b0e6b5d62a195fa2679045f203c3077a7574595
Dalton Barreto <daltonmatos@gmail.com>
2013-11-01 10:58:14 -0200

    Atualizando a linha do processo que mudou de estado

--- app/scripts/controllers/ServerDetail.js
+++ app/scripts/controllers/ServerDetail.js
@@ -9,10 +9,8 @@ angular.module('dashSupervisorFrontApp')
       $scope._open_state = {};
 
       $scope._refresh = function (){
-          console.log("Refreshing process list...");
           var future = $http({method: "GET",url:"/server/"+$scope._id+"/process"})
               .success(function (data, status){
-                    console.log(data);
                     $scope._by_groupname = $scope._divide_by_groupname(data);
                     $scope.process = data;
               })
@@ -36,15 +34,34 @@ angular.module('dashSupervisorFrontApp')
         return _by_groupname;
       };
 
-      $scope._update_internal = function (){};
+      $scope._update_internal = function (action, procname){
+          var groupname = procname.split(":")[0];
+          var name = procname.split(":")[1];
+          console.log(groupname);
+          console.log($scope._by_groupname);
+          for (var key in $scope._by_groupname){
+            if (key == groupname){
+                console.log($scope._by_groupname[groupname]);
+                $scope._by_groupname[groupname].forEach(function (o){
+                    if (o.name == name){
+                        if (action == "stop"){
+                            o.statename = "STOPPED";
+                        }else{
+                            o.statename = "RUNNING";
+                        }
+                    }
+                });
+            }
+          }
+      };
 
       $scope._action_on_process = function(action, procname){
             console.log(action+" Process: "+procname);
             $http({method: "POST",url:"/server/"+$scope._id+"/"+action, data: {process:procname}})
                 .success(function (data, status){
-                    console.log(data);
                     $scope.alerts.push(data['data'] + " " + procname);
                     $scope.action_running = false;
+                    $scope._update_internal(action, procname);
                 })
                 .error(function (data, status){
                     $scope.alerts.push(data['data'] + " " + procname);
