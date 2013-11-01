02f4c37aeb2cb3dce5035e045b06c0fd54fd2198
Jesue Junior <jesuesousa@gmail.com>
2013-11-01 00:17:27 -0200

    retirando path do home

--- app.js
+++ app.js
@@ -34,7 +34,6 @@ if ('development' == app.get('env')) {
     app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
 app.get('/users', user.list);
 
 app.post('/server', function(req, res){
