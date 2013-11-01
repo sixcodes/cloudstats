3116d6c575d89fd4c01dba686719b81a9f314443
Thúlio Costa <thulio@thuliocosta.com>
2013-11-01 00:03:47 -0200

    Build and run scripts

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+PROJECT_DIR=$(dirname $0)
+npm install
+bower install
+
+pushd $PROJECT_DIR/app/bower_components/bootstrap-ui/ > /dev/null
+
+npm install
+grunt build
+
+popd > /dev/null
+
+pushd $PROJECT_DIR/public > /dev/null
+ln -sf ../app/* .
+
+popd > /dev/null
