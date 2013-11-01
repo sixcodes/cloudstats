0c170964c9bf546bf83fcb11dcd3a733e7d1937b
Dalton Barreto <daltonmatos@gmail.com>
2013-11-01 02:15:22 -0200

    Separate processes by groupname

--- app/scripts/controllers/ServerDetail.js
+++ app/scripts/controllers/ServerDetail.js
@@ -6,8 +6,21 @@ angular.module('dashSupervisorFrontApp')
       $http({method: "GET",url:"/server/"+$scope._id+"/process"})
           .success(function (data, status){
                 console.log(data);
+                $scope._by_groupname = $scope._divide_by_groupname(data);
                 $scope.process = data;
           })
           .error(function (data, status){
           });
+
+      $scope._divide_by_groupname = function(process_list){
+        console.log(process_list);
+        $scope._by_groupname = {};
+        process_list.forEach(function (p){
+            if (!$scope._by_groupname[p.group]){
+                $scope._by_groupname[p.group] = Array();
+            }
+            $scope._by_groupname[p.group].push(p);
+        });
+        console.log($scope._by_groupname);
+      };
   });
