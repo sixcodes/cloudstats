dde131303836497c208268a7af17154d96344075
Jesue Junior <jesuesousa@gmail.com>
2013-11-10 01:42:30 -0200

    Organizando rotas

--- routes/servers.js
+++ routes/servers.js
@@ -3,5 +3,10 @@ module.exports = function(app){
 
     app.post('/server', server.create);
     app.get('/server', server.getAll);
+    app.post('/server/:id/:action', server.executeAction);
+    app.get('/server/:id', server.getById);
+    app.get('/server/:id/process', server.getProcessById);
+    app.put('/server', server.update);
+    app.delete('/server/:id', server.delete);
 
 };
