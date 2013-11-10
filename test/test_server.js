28ce59fb2852835430410a939686ab2b7dc016eb
Jesue Junior <jesuesousa@gmail.com>
2013-11-10 02:08:32 -0200

    Iniciando os testes

--- test/test_server.js
+++ test/test_server.js
@@ -1,7 +1,11 @@
-/**
- * Created with JetBrains WebStorm.
- * User: jesuejunior
- * Date: 11/10/13
- * Time: 2:00 AM
- * To change this template use File | Settings | File Templates.
- */
+var app = require('../app'),
+    request = require('supertest')(app);
+
+describe("Testando retorno dos GET", function(){
+    it("Retorna JSON com todos dados do banco", function(done){
+        request.get('/server').end(function(err, res){
+            res.status.should.eql(200);
+            done();
+        });
+    });
+});
