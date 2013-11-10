ea7776a95686d3fce6ea75584c4780b57566f216
Jesue Junior <jesuesousa@gmail.com>
2013-11-10 02:07:51 -0200

    Centralizando conexão com o banco

---
+++ middleware/db_connect.js
@@ -0,0 +1,10 @@
+module.exports = function(){
+    var mongoose = require('mongoose');
+    var env_url = {
+        "test" : "mongodb://localhost:27017/test_cloudstats",
+        "development": "mongodb://localhost:27017/cloudstats"
+    };
+    var uristring = env_url[ process.env.NODE_ENV ||
+        "development" ];
+    return mongoose.connect(uristring);
+};
---
+++ test/test_server.js
@@ -0,0 +1,7 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: jesuejunior
+ * Date: 11/10/13
+ * Time: 2:00 AM
+ * To change this template use File | Settings | File Templates.
+ */
