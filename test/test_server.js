becb01094fdb3fa65832f21f7a3cd48bbe220a87
Jesue Junior <jesuesousa@gmail.com>
2013-12-04 20:23:02 -0200

    Tests back to normal, five passed

--- test/test_server.js
+++ test/test_server.js
@@ -3,36 +3,6 @@ var app = require('../app'),
     request = require('supertest')(app);
 var modelServer = app.models.servers;
 
-describe("Test return params GET", function(){
-
-    var serverItem = {
-        "_id": '52865e05ae84f66fee000001',
-        "name": "testes1",
-        "rpc_url": "updater.domain.com.br:9000",
-        "rpc_user": "ghost",
-        "rpc_pass": "12345",
-        "obs": "rá",
-        "admin_email": 'admin@cloudstats.com'
-    };
-
-    var serverItem2 = {
-        "_id": '52865e05ae84f66fee000002',
-        "name": "testes2",
-        "rpc_url": "updater2.domain.com.br:9000",
-        "rpc_user": "ghost",
-        "rpc_pass": "12345",
-        "obs": "rrá",
-        "admin_email": 'admin@cloudstats.com'
-    };
-
-    beforeEach(function(done){
-        var newServer = modelServer(serverItem);
-        newServer.save();
-        var newServer2 = modelServer(serverItem2);
-        newServer2.save();
-        done();
-    });
-
 //    afterEach(function(done){
 //        modelServer.find({}, function(err, data){
 //            data.forEach(function(item){
@@ -41,19 +11,54 @@ describe("Test return params GET", function(){
 //        });
 //        done();
 //    });
-        it("Retorna todos os JSON do banco", function(done){
-            request.get('/server').end(function(err, res){
-                res.status.should.eql(200);
-                assert.equal(res.body.length, 2);
-                assert.equal(res.body[0].name, 'testes1');
-                res.body[0].rpc_url.should.equal("updater.domain.com.br:9000");
-                assert.equal(res.body[0].rpc_user, 'ghost');
-                assert.equal(res.body[0].rpc_pass, '12345');
-                assert.equal(res.body[0].obs, 'rá');
-                assert(res.body[0].admin_email, 'admin@cloudstats.com');
-                done();
+
+describe("Test return params GET", function(){
+
+    before(function(done){
+
+        modelServer.find({}, function(err, data){
+            data.forEach(function(item){
+                item.remove();
             });
+
+            var serverItem = {
+                "_id": '52865e05ae84f66fee000001',
+                "name": "testes1",
+                "rpc_url": "updater.domain.com.br:9000",
+                "rpc_user": "ghost",
+                "rpc_pass": "12345",
+                "obs": "rá",
+                "admin_email": 'admin@cloudstats.com'
+            };
+
+            var serverItem2 = {
+                "_id": '52865e05ae84f66fee000002',
+                "name": "testes2",
+                "rpc_url": "updater2.domain.com.br:9000",
+                "rpc_user": "ghost",
+                "rpc_pass": "12345",
+                "obs": "rrá",
+                "admin_email": 'admin@cloudstats.com'
+            };
+
+            var newServer = modelServer(serverItem);
+            newServer.save();
+            var newServer2 = modelServer(serverItem2);
+            newServer2.save();
+
+            done();
         });
+
+    });
+
+
+    it("Retorna todos os JSON do banco", function(done){
+        request.get('/server').end(function(err, res){
+            res.status.should.eql(200);
+            assert.equal(res.body.length, 2);
+            done();
+        });
+    });
 
     it("Find server by ID", function(done){
         request.get('/server/52865e05ae84f66fee000001').end(function(err, res){
@@ -71,7 +76,8 @@ describe("Test return params GET", function(){
 });
 
 describe("Test POST to create a new server", function(done){
-    var serverItem = {
+
+    var serverIt = {
         "name": "testes_new",
         "rpc_url": "updater_new.domain.com.br:9000",
         "rpc_user": "ghost",
@@ -79,15 +85,75 @@ describe("Test POST to create a new server", function(done){
         "obs": "rá",
         "admin_email": 'admin2@cloudstats.com'
     };
+
     it("Create new", function(done){
-        request.post('/server').send(serverItem).end(function(err, res){
+        request.post('/server').send(serverIt).end(function(err, res){
             assert.equal(res.status, 200);
         });
+        done();
     });
 
     it("Checking new server", function(done){
-        modelServer.findOne({"name":"teste_new"}, function(err, data){
+        modelServer.findOne({"name":"testes_new"}, function(err, data){
             assert.equal(data.name, "testes_new");
-        })
+            assert.equal(data.rpc_url, "updater_new.domain.com.br:9000");
+            assert.equal(data.rpc_user, "ghost");
+            assert.equal(data.rpc_pass, "12345");
+            assert.equal(data.obs, "rá");
+            assert.equal(data.admin_email, "admin2@cloudstats.com");
+        });
+        done();
     })
 });
+
+describe("Test update server PUT", function(done){
+
+    beforeEach(function(done){
+        var server1 = {
+            "_id": '52865e05ae84f66fee000003',
+            "name": "server1",
+            "rpc_url": "updater1.domain.com.br:9000",
+            "rpc_user": "ghost",
+            "rpc_pass": "12345",
+            "obs": "rrá",
+            "admin_email": 'admin@cloudstats.com'
+        };
+
+        var newServer = modelServer(server1);
+        newServer.save();
+        done();
+    });
+
+    it("Update server1", function(done){
+        var serverNew = {
+            "_id": '52865e05ae84f66fee000003',
+            "name": "server123",
+            "rpc_url": "updater3.domain.com.br:9000",
+            "rpc_user": "ghosts",
+            "rpc_pass": "123456",
+            "obs": "rá",
+            "admin_email": 'admin@cloudstats.com'
+        };
+
+        request.put('/server').send(serverNew).end(function(err, res){
+            assert.equal(200, res.status);
+        });
+
+        modelServer.findOne({"_id":"52865e05ae84f66fee000003"}, function(err, data){
+            assert(data.name, 'server123');
+            assert(data.rpc_url, 'updater3.domain.com.br:9000');
+            assert(data.rpc_user, 'ghosts');
+            assert(data.rpc_pass, '123456');
+            assert(data.obs, 'rá');
+            assert(data.admin_email, 'admin@cloudstats.com');
+        });
+
+         done();
+    })
+});
+
+//describe("", function(done){
+//    it("", function(done){
+//
+//    })
+//});
