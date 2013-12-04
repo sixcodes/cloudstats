18e2931d854dda3250aad3a5ea3b8c2b7f832a05
Jesue Junior <jesuesousa@gmail.com>
2013-12-04 20:22:14 -0200

    ...

--- middleware/db_connect.js
+++ middleware/db_connect.js
@@ -4,7 +4,6 @@ module.exports = function(){
         "test" : "mongodb://localhost:27017/test_cloudstats",
         "development": "mongodb://localhost:27017/cloudstats"
     };
-    var uristring = env_url[ process.env.NODE_ENV ||
-        "development" ];
+    var uristring = env_url[ process.env.NODE_ENV || "development" ];
     return mongoose.connect(uristring);
 };
