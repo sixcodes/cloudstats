737cb65bb3ec48411367e0645ba07e2d61e6bd46
Dalton Barreto <daltonmatos@sieve.com.br>
2014-09-18 20:25:40 -0300

    Install pymysql as mysqldb

--- cloudstats/wsgi.py
+++ cloudstats/wsgi.py
@@ -8,6 +8,11 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
 import os
+
+# Instala o pymysql no lugar do MySQLdb
+import pymysql
+pymysql.install_as_MySQLdb()
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cloudstats.settings")
 
 from django.core.wsgi import get_wsgi_application
