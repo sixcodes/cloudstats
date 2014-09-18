43b24e53db6e24791644777e1ab43c15505b16f1
Dalton Barreto <daltonmatos@sieve.com.br>
2014-09-18 20:42:36 -0300

    Patching MySQLdb for manage.py

--- manage.py
+++ manage.py
@@ -2,6 +2,11 @@
 import os
 import sys
 
+# Instala o pymysql no lugar do MySQLdb
+import pymysql
+pymysql.install_as_MySQLdb()
+
+
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cloudstats.settings")
 
