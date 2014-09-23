7e929da068f3d201b57986f1faf324ea85f31b4a
Dalton Barreto <daltonmatos@sieve.com.br>
2014-09-23 15:21:44 -0300

    Adicionando uma view que requer autenticação
    IN-387

--- api/views.py
+++ api/views.py
@@ -1,11 +1,13 @@
 from django.contrib.auth.models import User, Group
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 from api.serializers import UserSerializer, GroupSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = (permissions.IsAuthenticated,)
+
 
 
 class GroupViewSet(viewsets.ModelViewSet):
