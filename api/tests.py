bc20b0f2303fc1e5e997d365083cb9eb9a4f9785
Dalton Barreto <daltonmatos@sieve.com.br>
2014-09-25 11:44:30 -0300

    Validação de criação de dois servers com mesmo IP de origem
    IN-390

--- api/tests.py
+++ api/tests.py
@@ -9,20 +9,40 @@ class ServerAPITest(TestCase):
 
     def setUp(self):
         self.u = User.objects.create_user("testuser", password="secret")
+        self.token = Token.objects.filter(user=self.u).all()[0]
         self.client = client.Client()
-
-    def test_create_new_server(self):
-        server_data = {
+        self.server_data = {
             "name": "etl",
             "ipaddress": "127.0.0.1",
             "supervisord_port": 9000,
             "supervisord_pwd": "sievera",
         }
-        token = Token.objects.filter(user=self.u).all()[0]
-        response = self.client.post(reverse('server-list'), content_type='application/json', data=json.dumps(server_data), HTTP_AUTHORIZATION="Token {}".format(token.key))
+
+    def test_create_new_server(self):
+        response = self.client.post(reverse('server-list'), content_type='application/json', data=json.dumps(self.server_data), HTTP_AUTHORIZATION="Token {}".format(self.token.key))
         self.assertEqual(201, response.status_code)
         etl_server = Server.objects.filter(name="etl").all()[0]
-        self.assertEqual(etl_server.name, server_data['name'])
-        self.assertEqual(etl_server.ipaddress, server_data['ipaddress'])
-        self.assertEqual(etl_server.supervisord_port, server_data['supervisord_port'])
-        self.assertEqual(etl_server.supervisord_pwd, server_data['supervisord_pwd'])
+        self.assertEqual(etl_server.name, self.server_data['name'])
+        self.assertEqual(etl_server.ipaddress, self.server_data['ipaddress'])
+        self.assertEqual(etl_server.supervisord_port, self.server_data['supervisord_port'])
+        self.assertEqual(etl_server.supervisord_pwd, self.server_data['supervisord_pwd'])
+
+    def test_donot_create_servers_with_same_ip_address(self):
+        server = Server(**self.server_data)
+        server.save()
+        response = self.client.post(reverse('server-list'), content_type='application/json', data=json.dumps(self.server_data), HTTP_AUTHORIZATION="Token {}".format(self.token.key))
+        self.assertEqual(400, response.status_code)
+
+    def test_donot_trust_data_ipaddress(self):
+        """
+        Get the IP address from the REMOTE_ADDR request header
+        :return:
+        """
+        self.server_data['ipaddress'] = '8.8.8.8'
+        response = self.client.post(reverse('server-list'), content_type='application/json', data=json.dumps(self.server_data), HTTP_AUTHORIZATION="Token {}".format(self.token.key))
+        servers = Server.objects.filter(ipaddress=self.server_data['ipaddress']).all().count()
+        self.assertEqual(0, servers)
+        self.assertEqual(201, response.status_code)
+        server = Server.objects.all()[0]
+        self.assertEqual("127.0.0.1", server.ipaddress)
+
--- api/views.py
+++ api/views.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.models import User
 from rest_framework import viewsets, permissions
+from rest_framework.response import Response
+from rest_framework import status
 
 from api.models import Server
 from api.serializers import UserSerializer, ServerSerializer
@@ -15,3 +17,10 @@ class ServerView(viewsets.ModelViewSet):
     permission_classes = (permissions.IsAuthenticated,)
     serializer_class = ServerSerializer
     queryset = Server.objects.all()
+
+    def create(self, request, *args, **kwargs):
+        remote_addr = request.META.get('REMOTE_ADDR', None)
+        if Server.objects.filter(ipaddress=remote_addr).exists():
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        request.DATA['ipaddress'] = remote_addr
+        return super(viewsets.ModelViewSet, self).create(request, *args, **kwargs)
