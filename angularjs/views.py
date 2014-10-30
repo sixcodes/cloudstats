96abaad364c4364082e6d33a82f37433c290bbd5
Dalton Barreto <daltonmatos@sieve.com.br>
2014-10-30 18:28:34 -0200

    Exchange cookie for token

--- angularjs/views.py
+++ angularjs/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render_to_response
+from rest_framework.authentication import Token
+from rest_framework.response import Response
+from rest_framework.status import HTTP_403_FORBIDDEN
 
 
 def index(request):
     return render_to_response('index.html')
+
+
+def token(request):
+    if request.user.is_authenticated():
+        user_token = Token.objects.get(user=request.user)
+        return Response(data={"user_token": user_token.key}, status=200)
+    return Response(status=HTTP_403_FORBIDDEN)
--- cloudstats/urls.py
+++ cloudstats/urls.py
@@ -15,6 +15,7 @@ router.register(r"stats", views.StatsView)
 urlpatterns = patterns('',
     url(r'^social/', include('social.apps.django_app.urls', namespace='social')),
     url(r'^$', 'angularjs.views.index'),
+    url(r'^token$', 'angularjs.views.token'),
     url(r'^api-login', 'rest_framework.authtoken.views.obtain_auth_token', name='get-token'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^docs/', include('rest_framework_swagger.urls')),
