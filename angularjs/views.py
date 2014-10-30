e7576b7880a8e857d620c351147b4116349be8b9
Dalton Barreto <daltonmatos@sieve.com.br>
2014-10-30 17:09:19 -0200

    Revert "Auto-login na raiz da app"
    This reverts commit c59219a56990a4cc2a78c82dd8b03bb99077d44e.

--- angularjs/views.py
+++ angularjs/views.py
@@ -1,7 +1,5 @@
-from django.shortcuts import render_to_response, redirect
-from django.conf import settings
+from django.shortcuts import render_to_response
+
 
 def index(request):
-    if request.user.is_authenticated():
-        return redirect(settings.SOCIAL_AUTH_LOGIN_REDIRECT_URL)
     return render_to_response('index.html')
