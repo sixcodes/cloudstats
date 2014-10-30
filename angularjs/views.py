b2db029411217fd96fc12b45a3741618d260e927
Dalton Barreto <daltonmatos@sieve.com.br>
2014-10-30 17:03:28 -0200

    Auto-login na raiz da app

--- angularjs/views.py
+++ angularjs/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render_to_response
-
+from django.shortcuts import render_to_response, redirect
+from django.conf import settings
 
 def index(request):
+    if request.user.is_authenticated():
+        return redirect(settings.SOCIAL_AUTH_LOGIN_REDIRECT_URL)
     return render_to_response('index.html')
