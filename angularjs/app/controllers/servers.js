cb2e227e378715910258038722b6ffa8a6d5d649
Dalton Barreto <daltonmatos@sieve.com.br>
2014-11-18 18:54:54 -0200

    Limpando controller/servers.js

--- angularjs/app/controllers/servers.js
+++ angularjs/app/controllers/servers.js
@@ -10,45 +10,10 @@
             $scope.servers = data.results;
         });
 
-        this.start = function(server, process){
-            ProcessService.start(server, process, function(data){
-                angular.copy(data, process);
-            });
-        };
-
-        this.stop = function(server, process){
-            ProcessService.stop(server, process, function(data){
-                angular.copy(data, process);
-            });
-        };
-
-        this.restart = function(server, process){
-            ProcessService.restart(server, process, function(data){
-                angular.copy(data, process);
-            });
-        };
-
-        this.all_processes = function(server){
-            ProcessService.all(server).$promise.then(function(data){
-                $scope.server_processes[server.id] = {};
-                angular.forEach(data, function(item){
-                    var proc = ProcessInstance(item);
-                    $scope.server_processes[server.id][proc.name] = proc;
-                });
-            });
-        };
-
         this.get_detail = function(server){
             $location.path("/servers/" + server.id + "/processes");
         };
 
-        this.is_running = function(statename){
-            return (statename === 'RUNNING');
-        };
-
-        this.can_interact_with_process = function(process){
-            return process.can_interact;
-        };
     });
 
 })();
